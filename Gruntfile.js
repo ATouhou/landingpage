a6ad47136d13798da04c0b89e21eedf43be3c187
Mattias Hising <hising@gmail.com>
2014-03-13 11:06:28 +0100

    now with grunt sass and a hero unit

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+    watch: {
+			sass: {
+				files: ['scss/**/*.scss'],
+				tasks: ['sass:dist']
+			}
+		},
+
+		sass: {
+			dist: {
+				files: {
+					'css/style.css': 'scss/style.scss'
+				}
+			}
+		}
+
+	});
+
+	grunt.registerTask('default', ['sass:dist', 'watch']);
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+};
