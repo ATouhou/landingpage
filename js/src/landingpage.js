c75a5d9cdd3e81ebbc8e52f1c0910f4b4f357870
Mattias Hising <hising@gmail.com>
2014-11-13 11:32:23 +0100

    Fixed browserify, signup modal and a testimonial row

---
+++ js/src/landingpage.js
@@ -0,0 +1,15 @@
+var Landingpage = {};
+
+Landingpage.init = function () {
+
+	$(document).foundation();
+
+	$(function () {
+		console.log("loaded");
+	});
+};
+
+Landingpage.init();
+
+
+module.exports = Landingpage;
