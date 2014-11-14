57fbe90a24c42fda3b823f2536a62e92a8de067f
Mattias Hising <hising@gmail.com>
2014-11-14 15:22:51 +0100

    Added support for device classes as html classes

--- js/src/device.js
+++ js/src/device.js
@@ -1,4 +1,3 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var Device = function (userAgent) {
     this.userAgent = userAgent;
     this.deviceClasses = [];
@@ -152,28 +151,3 @@ var Device = function (userAgent) {
 };
 
 module.exports = Device;
-},{}],2:[function(require,module,exports){
-module.exports=require(1)
-},{"/Users/hising/www/landingpage/js/src/Device.js":1}],3:[function(require,module,exports){
-var Device = require('./Device');
-var Landingpage = {};
-
-var root = $('html');
-root.removeClass('no-js');
-
-var device = new Device(navigator.userAgent);
-var deviceClasses = device.getDeviceClasses();
-
-deviceClasses.forEach(function (item){ root.addClass(item); });
-
-Landingpage.init = function () {
-	$(document).foundation();
-	$(function () {});
-};
-
-Landingpage.init();
-
-
-module.exports = Landingpage;
-
-},{"./Device":1}]},{},[2,3]);
--- js/src/landingpage.js
+++ js/src/landingpage.js
@@ -1,12 +1,17 @@
+var Device = require('./Device');
 var Landingpage = {};
 
+var root = $('html');
+root.removeClass('no-js');
+
+var device = new Device(navigator.userAgent);
+var deviceClasses = device.getDeviceClasses();
+
+deviceClasses.forEach(function (item){ root.addClass(item); });
+
 Landingpage.init = function () {
-
 	$(document).foundation();
-
-	$(function () {
-		console.log("loaded");
-	});
+	$(function () {});
 };
 
 Landingpage.init();
